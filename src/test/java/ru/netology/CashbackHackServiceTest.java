57651afb3e7ff6b874e9668e217a96e2e8b373eb
Igor <TimoshenkoII@yandex.com>
2023-05-16 18:23:28 +0300

    Changes commit

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,49 +1,45 @@
 package ru.netology;
 
-
 public class CashbackHackServiceTest {
-    @org.testng.annotations.Test
+    @org.junit.Test
     /// При нулевой покупке
     public void CashbackHackServiceTestFirst() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertEquals(cashbackHackService.remain(0),1000);
+        org.junit.Assert.assertEquals(cashbackHackService.remain(0), 1_000);
     }
-    @org.testng.annotations.Test
+
+    @org.junit.Test
     /// Стоимость покупки составляет  1 рубль
     public void CashbackHackServiceTestSecond() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertEquals(cashbackHackService.remain(1),999);
+        org.junit.Assert.assertEquals(cashbackHackService.remain(1), 999);
     }
-    @org.testng.annotations.Test
-    /// Ограничение на отрицательную покупку / Баг
+
+    @org.junit.Test
+    /// Стоимость покупки составляет  549 рублей
     public void CashbackHackServiceTestThird() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertThrows(RuntimeException.class, () ->{
-            cashbackHackService.remain(-1);
-        });
+        org.junit.Assert.assertEquals(cashbackHackService.remain(476), 524);
     }
-    @org.testng.annotations.Test
-    /// Стоимость покупки составляет  549 рублей
+
+    @org.junit.Test
+    /// Стоимость покупки составляет  999 рублей
     public void CashbackHackServiceTestFourth() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertEquals(cashbackHackService.remain(549),451);
+        org.junit.Assert.assertEquals(cashbackHackService.remain(999), 1);
     }
-    @org.testng.annotations.Test
-    /// Стоимость покупки составляет  999 рублей
+
+    @org.junit.Test
+    /// Стоимость покупки составляет  1000 рублей / Баг
     public void CashbackHackServiceTestFifth() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertEquals(cashbackHackService.remain(999),1);
+        org.junit.Assert.assertEquals(cashbackHackService.remain(1_000), 0);
     }
-    @org.testng.annotations.Test
-    /// Стоимость покупки составляет  1000 рублей / Баг
+
+    @org.junit.Test
+    /// Стоимость покупки составляет  1001 рублей
     public void CashbackHackServiceTestSixth() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertEquals(cashbackHackService.remain(1000),0);
-    }
-    @org.testng.annotations.Test
-    /// Стоимость покупки составляет  1001 рублей
-    public void CashbackHackServiceTestSeventh() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        org.testng.Assert.assertEquals(cashbackHackService.remain(1001),999);
+        org.junit.Assert.assertEquals(cashbackHackService.remain(1_001), 999);
     }
 }
